7b1d50dd3ee5c2910d24704bc04ba2353944d82a
Bourelle Frédéric <frederic.bourelle@gmail.com>
2019-03-01 15:51:52 +0100

    update sautemonton

--- SauteMouton.java
+++ SauteMouton.java
@@ -1,125 +1,286 @@
+package fr.eni.tp.tableaux.sautemouton;
 import java.util.Scanner;
-
-public class SauteMouton {
-
-	public static final int NB_MOUTONS = 3;
-	public static final int NB_CASES = 2 * NB_MOUTONS + 1;
+/**
+ * Correction SauteMouton.java
+ * @author fbourell2018
+ *
+ */
+public class TP_Tableau_SauteMouton {
+	public static char moutonGauche = 'm';
+	public static char moutonDroite = 'n';
+	public static char placeLibre = '_';
+	public static char[] tabMoutons = { moutonGauche, moutonGauche, moutonGauche, placeLibre, moutonDroite,
+			moutonDroite, moutonDroite };
+	public static Scanner scan = new Scanner(System.in);
+	public static int gameover = -1;
 
 	public static void main(String[] args) {
+		// final int fin = -1;
+		// bouclesPair();
+		// boucleCentre();
+		// bouclesImpair();
+		System.out.println(tabMoutons.length / 2);
 
-		char[] plateau = new char[NB_CASES];
-		initialiser(plateau);
-		do {
-			afficher(plateau);
-			deplacerUnMouton(plateau);
-		} while (!gagne(plateau) && !perdu(plateau));
-		afficher(plateau);
-	}
+		while (gameover < 0) {
+			afficheTabMouton();
 
-	public static boolean perdu(char[] plateau) {
-		boolean deplPossible;
-		int posCaseVide = 0;
-		while(plateau[posCaseVide]!=' ')
-			posCaseVide++;
-		if(posCaseVide>0 && plateau[posCaseVide-1] == '>')
-			deplPossible = true;
-		else if(posCaseVide>1 && plateau[posCaseVide-1] == '<' &&
-				plateau[posCaseVide-2] == '>')
-			deplPossible = true;
-		else if(posCaseVide<NB_CASES-1 && plateau[posCaseVide+1]== '<')
-			deplPossible = true;
-		else if(posCaseVide<NB_CASES-2 && plateau[posCaseVide+1]== '>' &&
-				plateau[posCaseVide+2]== '<')
-			deplPossible = true;
-		else
-			deplPossible = false;
-		if(!deplPossible)
-			System.out.println("C'est perdu !");
-		return !deplPossible;
-	}
+			System.out.println(
+					"Choisissez un chiffre entre 0 et " + (tabMoutons.length - 1) + " pour déplacer un mouton");
+			int choix = scan.nextInt();
+			scan.nextLine();
 
-	public static boolean gagne(char[] plateau) {
-		boolean victoire = true;
-		int i=0;
-		while(i < NB_MOUTONS && victoire) {
-			victoire = plateau[i] == '<';
-			i++;
-		}
-		victoire = victoire && plateau[NB_MOUTONS] == ' ';
-		if(victoire)
-			System.out.println("Victoire !!!!");
-		return victoire;
-	}
-
-	private static void deplacerUnMouton(char[] plateau) {
-		int numeroCase = saisieEntierEntreBornes(1, NB_CASES, "Quel mouton voulez-vous déplacer ?") - 1;
-		switch (plateau[numeroCase]) {
-		case ' ':
-			System.out.println("Il n'y a pas mouton ici !");
-			break;
-		case '>':
-			if (numeroCase + 1 < NB_CASES && plateau[numeroCase + 1] == ' ') {
-				plateau[numeroCase] = ' ';
-				plateau[numeroCase + 1] = '>';
-			} else if (numeroCase + 2 < NB_CASES && plateau[numeroCase + 1] == '<' && plateau[numeroCase + 2] == ' ') {
-				plateau[numeroCase] = ' ';
-				plateau[numeroCase + 2] = '>';
-			} else {
-				System.out.println("Déplacement illégal !");
+			while (choix < 0 || choix >= tabMoutons.length) {
+				System.out.println(
+						"Choisissez un chiffre entre 0 et " + (tabMoutons.length - 1) + " pour déplacer un mouton");
+				choix = scan.nextInt();
+				scan.nextLine();
 			}
-			break;
-		case '<':
-			if (numeroCase - 1 >= 0 && plateau[numeroCase - 1] == ' ') {
-				plateau[numeroCase] = ' ';
-				plateau[numeroCase - 1] = '<';
-			} else if (numeroCase - 2 >= 0 && plateau[numeroCase - 1] == '>' && plateau[numeroCase - 2] == ' ') {
-				plateau[numeroCase] = ' ';
-				plateau[numeroCase - 2] = '<';
+
+			if (tabMoutons[choix] == moutonGauche) {
+				if (choix + 1 < tabMoutons.length || choix + 2 < tabMoutons.length) {
+					if (tabMoutons[choix + 1] == placeLibre) {
+						tabMoutons[choix + 1] = tabMoutons[choix];
+						tabMoutons[choix] = placeLibre;
+						isWin();
+						isGameOver(choix);
+						// isGameOverGauche(choix);
+					} else if (tabMoutons[choix + 2] == placeLibre) {
+						tabMoutons[choix + 2] = tabMoutons[choix];
+						tabMoutons[choix] = placeLibre;
+						isWin();
+						isGameOver(choix);
+						// isGameOverGauche(choix);
+					} else {
+						System.out.println("Ce mouton ne peut pas se déplacer");
+					}
+				} else {
+					System.out.println("Ce mouton ne peut pas se déplacer");
+				}
+			} else if (tabMoutons[choix] == placeLibre) {
+				System.out.println("Ce n'est pas un mouton");
 			} else {
-				System.out.println("Déplacement illégal !");
+				if (choix - 1 >= 0 || choix - 2 > 0) {
+					if (tabMoutons[choix - 1] == placeLibre) {
+						tabMoutons[choix - 1] = tabMoutons[choix];
+						tabMoutons[choix] = placeLibre;
+						isWin();
+						isGameOver(choix);
+						// isGameOverDroite(choix);
+					} else if (tabMoutons[choix - 2] == placeLibre) {
+						tabMoutons[choix - 2] = tabMoutons[choix];
+						tabMoutons[choix] = placeLibre;
+						isWin();
+						isGameOver(choix);
+						// isGameOverDroite(choix);
+					} else {
+						System.out.println("Ce mouton ne peut pas se déplacer");
+					}
+				} else {
+					System.out.println("Ce mouton ne peut pas se déplacer");
+				}
 			}
-			break;
 		}
-	}
-	
-	
 
-	public static void afficher(char[] plateau) {
-		for (int i = 0; i < NB_CASES; i++)
-			System.out.print(plateau[i]);
-		System.out.println();
+		afficheTabMouton();
+		if (gameover == 1) {
+			System.out.println("Vous avez gagné !!");
+		} else {
+			System.out.println("Vous avez perdu !!");
+		}
 
-	}
-
-	public static void initialiser(char[] plateau) {
-		for (int i = 0; i < NB_MOUTONS; i++)
-			plateau[i] = '>';
-		plateau[NB_MOUTONS] = ' ';
-		for (int i = NB_MOUTONS + 1; i < NB_CASES; i++)
-			plateau[i] = '<';
+		scan.close();
 	}
 
-	public static Scanner scan = new Scanner(System.in);
 	/**
-	 * Demande à l'utilisateur de saisir une valeur entière comprise entre
-	 * {@code min} et {@code max}
-	 * 
-	 * @param min
-	 *            minimum acceptable (valeur incluse)
-	 * @param max
-	 *            maximum acceptable (valeur incluse)
-	 * @param message
-	 *            message à afficher pour inviter l'utilisateur à saisir
-	 * @return la valeur saisie par l'utilisateur forcément comprise entre
-	 *         {@code min} et {@code max}
+	 * Teste si l'utilisateur a gagné
 	 */
-	public static int saisieEntierEntreBornes(int min, int max, String message) {
-		System.out.println(message);
-		int j = scan.nextInt();
-		while (j < min || j > max) {
-			System.out.println("Veuillez saisir un nombre compris entre " + min + " et " + max);
-			j = scan.nextInt();
+	public static void isWin() {
+		if (tabMoutons[0] == moutonDroite && tabMoutons[1] == moutonDroite && tabMoutons[2] == moutonDroite
+				&& tabMoutons[4] == moutonGauche && tabMoutons[5] == moutonGauche && tabMoutons[6] == moutonGauche) {
+			gameover = 1;
 		}
-		return j;
 	}
+
+	/**
+	 * Vérifie si les déplacements ne sont plus possibles
+	 * 
+	 * @param choix
+	 *            la dernière case qui est vide
+	 */
+	public static void isGameOver(int choix) {
+		if (gameover != 1) {
+			if (choix > tabMoutons.length / 2) {
+				if (tabMoutons[choix - 1] == moutonDroite && tabMoutons[choix - 2] == moutonDroite
+						&& tabMoutons[choix - 3] == moutonDroite)
+					gameover = 0;
+			} else if (choix < tabMoutons.length / 2) {
+				if (tabMoutons[choix + 1] == moutonGauche && tabMoutons[choix + 2] == moutonGauche
+						&& tabMoutons[choix + 3] == moutonGauche)
+					gameover = 0;
+			} else {
+				if (tabMoutons[choix + 1] == moutonGauche && tabMoutons[choix + 2] == moutonGauche
+						&& tabMoutons[choix - 1] == moutonDroite && tabMoutons[choix - 2] == moutonDroite)
+					gameover = 0;
+			}
+		}
+	}
+
+	/**
+	 * Teste si l'utilisateur a perdu lorsqu'il déplace un mouton moutonGauche
+	 * 
+	 * @param {@code choix} est la case qui est vide placeLibre
+	 */
+	public static void isGameOverGauche(int choix) {
+		if (gameover != 1) {
+			if (choix == 0) {
+				if (tabMoutons[choix + 1] == moutonGauche && tabMoutons[choix + 2] == moutonGauche)
+					gameover = 0;
+			} else if (choix > 1 && choix < tabMoutons.length - 2) {
+				if (tabMoutons[choix + 1] == moutonGauche && tabMoutons[choix + 2] == moutonGauche
+						&& tabMoutons[choix - 1] == moutonDroite && tabMoutons[choix - 2] == moutonDroite)
+					gameover = 0;
+			}
+		}
+	}
+
+	/**
+	 * Teste si l'utilisateur a perdu lorsqu'il déplace un mouton moutonDroite
+	 * 
+	 * @param {@code choix} est la case qui est vide placeLibre
+	 */
+	public static void isGameOverDroite(int choix) {
+		if (gameover != 1) {
+			if (choix == tabMoutons.length - 1) {
+				if (tabMoutons[choix - 1] == moutonDroite && tabMoutons[choix - 2] == moutonDroite)
+					gameover = 0;
+			} else if (choix > 1 && choix < tabMoutons.length - 2) {
+				if (tabMoutons[choix + 1] == moutonGauche && tabMoutons[choix + 2] == moutonGauche
+						&& tabMoutons[choix - 1] == moutonDroite && tabMoutons[choix - 2] == moutonDroite)
+					gameover = 0;
+			}
+		}
+	}
+
+	/**
+	 * Affiche le tableau de moutons
+	 */
+	public static void afficheTabMouton() {
+		for (int i = 0; i < tabMoutons.length; i++) {
+			System.out.print(i);
+		}
+		System.out.println();
+		for (char mouton : tabMoutons) {
+			System.out.print(mouton);
+		}
+		System.out.println();
+	}
+
+	/*
+	 * 1er travail, résolution barbare par l'ordi
+	 */
+
+	public static void boucleCentre() {
+		int j1 = tabMoutons.length;
+		while (tabMoutons[j1 - 2] != placeLibre) {
+			j1--;
+		}
+		tabMoutons[j1 - 2] = tabMoutons[j1];
+		tabMoutons[j1] = placeLibre;
+		afficheTabMouton();
+	}
+
+	public static void bouclesPair() {
+		// Boucle 1
+		int j1 = 0;
+		while (tabMoutons[j1 + 1] != placeLibre) {
+			j1++;
+		}
+		tabMoutons[j1 + 1] = tabMoutons[j1];
+		tabMoutons[j1] = placeLibre;
+		afficheTabMouton();
+		tabMoutons[j1] = tabMoutons[j1 + 2];
+		tabMoutons[j1 + 2] = placeLibre;
+		afficheTabMouton();
+
+		// Boucle 2
+		int j2 = tabMoutons.length;
+		while (tabMoutons[j2 - 1] != placeLibre) {
+			j2--;
+		}
+		tabMoutons[j2 - 1] = tabMoutons[j2];
+		tabMoutons[j2] = placeLibre;
+		afficheTabMouton();
+		tabMoutons[j2] = tabMoutons[j2 - 2];
+		tabMoutons[j2 - 2] = placeLibre;
+		afficheTabMouton();
+
+		// Boucle 3
+		int j3 = 0;
+		while (tabMoutons[j3 + 2] != placeLibre) {
+			j3++;
+		}
+		tabMoutons[j3 + 2] = tabMoutons[j3];
+		tabMoutons[j3] = placeLibre;
+		afficheTabMouton();
+		tabMoutons[j3] = tabMoutons[j3 - 1];
+		tabMoutons[j3 - 1] = placeLibre;
+		afficheTabMouton();
+
+		// Boucle 4
+		int j4 = tabMoutons.length;
+		while (tabMoutons[j4 - 2] != placeLibre) {
+			j4--;
+		}
+		tabMoutons[j4 - 2] = tabMoutons[j4];
+		tabMoutons[j4] = placeLibre;
+		afficheTabMouton();
+	}
+
+	public static void bouclesImpair() {
+		// boucle 1
+		int j1 = tabMoutons.length;
+		while (tabMoutons[j1 - 2] != placeLibre) {
+			j1--;
+		}
+		tabMoutons[j1 - 2] = tabMoutons[j1];
+		tabMoutons[j1] = placeLibre;
+		afficheTabMouton();
+		tabMoutons[j1] = tabMoutons[j1 - 1];
+		tabMoutons[j1 - 1] = placeLibre;
+		afficheTabMouton();
+
+		// boucle 2
+		int j2 = 0;
+		while (tabMoutons[j2 + 2] != placeLibre) {
+			j2++;
+		}
+		tabMoutons[j2 + 2] = tabMoutons[j2];
+		tabMoutons[j2] = placeLibre;
+		afficheTabMouton();
+		tabMoutons[j2] = tabMoutons[j2 + 2];
+		tabMoutons[j2 - 2] = placeLibre;
+		afficheTabMouton();
+
+		// boucle 3
+		int j3 = tabMoutons.length;
+		while (tabMoutons[j3 - 1] != placeLibre) {
+			j3--;
+		}
+		tabMoutons[j3 - 1] = tabMoutons[j3];
+		tabMoutons[j3] = placeLibre;
+		afficheTabMouton();
+		tabMoutons[j3] = tabMoutons[j3 + 2];
+		tabMoutons[j3 + 2] = placeLibre;
+		afficheTabMouton();
+
+		// boucle 4
+		int j4 = 0;
+		while (tabMoutons[j4 + 1] != placeLibre) {
+			j4++;
+		}
+		tabMoutons[j4 + 1] = tabMoutons[j4];
+		tabMoutons[j4] = placeLibre;
+		afficheTabMouton();
+	}
+
 }
